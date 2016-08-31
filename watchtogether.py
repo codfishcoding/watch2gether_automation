c92b1a3aeb3605247df09fc0f231a1d93baaca75
codfishcoding <codfishcoding@gmail.com>
2016-08-30 22:29:56 -0400

    Added some comments to watchtogether.py
    adds comments to explain the code for when I or anyone else needs to modify this code

--- watchtogether.py
+++ watchtogether.py
@@ -4,16 +4,16 @@ import time
 import string
 
 driver = webdriver.Firefox()
-driver.get('https://www.watch2gether.com')
+driver.get('https://www.watch2gether.com') #going to site
 driver.find_element_by_css_selector('.ui.primary.button').click()
 
 
-time.sleep(3)
+time.sleep(3) #lets the browser load just as a precaution
 
 watch = driver.current_url
 
 driver.get("https://facebook.com")
-time.sleep(2)
+time.sleep(2) #sleeps because sometimes will not load fast enough
 
 #login to facebook
 driver.find_element_by_id('email').send_keys('notmyemail@hotmail.com')
@@ -22,11 +22,14 @@ driver.find_element_by_id('loginbutton').click()
 
 time.sleep(3)
 
+#this gets the class name to click on the message box on
+#the right hand side of facebook
 driver.find_element_by_class_name("class_of_friend").click()
 
 time.sleep(3)
 
+#this sends the keys into the message box once it has been created
 driver.find_element_by_css_selector('css_selector_of_friend').send_keys(str(watch) , Keys.ENTER )
 
-
+#go back to watch2gether
 driver.get(watch)
